099049830c09988e87543564abdceb62867e8d8d
SachinthaLakshan <sachintha.kahagalla@gmail.com>
2025-12-22 23:30:17 +0530

    socket not working issue fixed

--- frontend/app/provider/socket-context.tsx
+++ frontend/app/provider/socket-context.tsx
@@ -22,12 +22,12 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
         if (user) {
             const newSocket = io(import.meta.env.VITE_SOCKET_URL || "http://localhost:5000", {
                 query: { userId: user._id },
-                transports: ["websocket", "polling"],
-                withCredentials: true,
-            });
+                //     transports: ["websocket", "polling"],
+                //     withCredentials: true,
+                // });
 
-            newSocket.on("connect_error", (err) => {
-                console.error("Socket connection error:", err.message);
+                // newSocket.on("connect_error", (err) => {
+                //     console.error("Socket connection error:", err.message);
             });
 
             setSocket(newSocket);
